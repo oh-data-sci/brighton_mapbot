33ec9fdccf92c7625241baca5663e9470cfa8094
oskar <skarinn@yahoo.com>
2022-04-22 00:15:57 +0100

    minor

--- src/eda_postcodes.R
+++ src/eda_postcodes.R
@@ -11,15 +11,17 @@ filepaths <- c(
 
 # randomly choose one brightonian area and load their post codes
 filepath = sample(filepaths, 1) # brighton: latitude=50.82253 and longitude=-0.137163
-post_code_df <- fetch_postcode_data(filepath)
+post_code_df <- fetch_postcode_coordinates(filepath)
 print(paste("file", filepath, 'contains', nrow(post_code_df), 'entries'))
 
 print(paste('latitudes:  from', min(post_code_df$Latitude),  'to', max(post_code_df$Latitude)))
 print(paste('longitudes: from', min(post_code_df$Longitude), 'to', max(post_code_df$Longitude)))
 
+hist(post_code_df$"Average Income")
 hist(post_code_df$Latitude)
 hist(post_code_df$Longitude)
 plot(post_code_df$Longitude, post_code_df$Latitude)
+plot(post_code_df$"Average Income", post_code_df$Altitude)
 chosen_point_tb = choose_point_coordinates(post_code_df)
 print(chosen_point_tb)
 
