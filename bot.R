9341f5fa63d31323760d5532d3eef87530133118
oskar <skarinn@yahoo.com>
2022-04-22 00:13:28 +0100

    bot script. runs automatically now.

--- bot.R
+++ bot.R
@@ -6,16 +6,16 @@ source(file='src/prep_coordinates.R')
 source(file='src/send_mapbox_requests.R')
 source(file='src/send_tweet.R')
 
-# brighton and hove postcodes
-filepaths <- c(
-  'data/BN1_postcodes.csv', # brighton
-  'data/BN2_postcodes.csv', # kemptown-marina-rottingdean
-  'data/BN3_postcodes.csv'  # hove
-)
+filepaths <- list.files(path="data", pattern=".*_postcodes.csv")
+  # brighton and hove postcodes from: https://www.doogal.co.uk/UKPostcodes.php
+  # 'data/BN1_postcodes.csv', - brighton
+  # 'data/BN2_postcodes.csv', - kemptown-marina-rottingdean
+  # 'data/BN3_postcodes.csv'  - hove
 
-# randomly choose one brightonian area 
-filepath = sample(filepaths, 1) # brighton: lati=50.82253 and long=-0.137163
+# randomly choose one brightonian area file
+filepath = sample(filepaths, 1)
 
+# read postcode data file in csv format.
 post_code_df <- fetch_postcode_coordinates(filepath)
 random_point_tb <- dplyr::slice_sample(post_code_df,n=1)
 
@@ -23,7 +23,8 @@ image_filepath <- fetch_image_of_coordinates_from_mapbox(
   random_point_tb$Latitude, 
   random_point_tb$Longitude,
   access_token=Sys.getenv("MAPBOX_PUBLIC_ACCESS_TOKEN"))
-print('posting a tweet!')
+
+print('posting a tweet!', random_point_tb, image_filepath)
 
 post_mapbot_tweet(random_point_tb, image_filepath)
 
